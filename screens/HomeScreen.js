b0ad78004c71e4da8b20572490fd00180d136fb9
Nathanael Geier <nathanaelgeier@Nathanaels-MacBook-Pro.local>
2018-05-25 22:10:21 +0800

    First push

---
+++ screens/HomeScreen.js
@@ -0,0 +1,186 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+import styled from 'styled-components';
+
+import { MonoText } from '../components/StyledText';
+const Input = styled.TextInput`
+  background-color: #eeeeee;
+  padding: 15px;
+  margin: 15px;
+  height: 200px;
+  font-size: 32px;
+  color: 'rgba(88, 92, 92, 1)';
+`;
+
+const Header = styled.Text`
+  color: 'rgba(88, 92, 92, 1)';
+  padding: 5px;
+  margin: 5px;
+  font-size: 42px;
+`;
+
+const ButtonText = styled.Text`
+  color: #ffffff;
+  padding: 5px;
+  margin: 5px;
+  font-size: 22px;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: 'rgba(63, 70, 70, 1)';
+  flex: 1;
+  align-items: center;
+`;
+
+const Container = styled.View`
+  background-color: 'rgba(37, 46, 46, 1)';
+  flex: 1;
+`;
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  _onPressButton() {
+    console.log('winner');
+  }
+
+  render() {
+    return (
+      <Container style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/robot-dev.png')
+                  : require('../assets/images/robot-prod.png')
+              }
+              style={styles.welcomeImage}
+            />
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            <Header adjustsFontSizeToFit style={styles.getStartedText}>
+              TaJot
+            </Header>
+          </View>
+          <Input multiline defaultValue="Jot about today!" />
+          <Button onPress={this._onPressButton}>
+            <ButtonText>Push to Blockchain</ButtonText>
+          </Button>
+        </ScrollView>
+      </Container>
+    );
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/development-mode',
+    );
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes',
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
